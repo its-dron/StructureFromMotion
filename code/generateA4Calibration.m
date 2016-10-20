9592f4fbf02b1dd123b913c9c72f8ebaa9d11dd4
Brian Pugh <Brian Pugh>
2016-10-20 19:59:17 -0400

    Generates PDF document containing a test image where each checkerboard square has side length 2cm.

---
+++ code/generateA4Calibration.m
@@ -0,0 +1,14 @@
+function generateA4Calibration(destFilename)
+    h = figure;
+    nBoxes = 10;
+    squareSideLength = 2;
+    
+    imPos = [0, 0, squareSideLength*nBoxes, squareSideLength*nBoxes];
+    im = checkerboard(nBoxes);
+    im = imresize(im, 50, 'nearest');
+    imshow(im);
+    set(h, ...
+        'PaperUnits','centimeters', ...
+        'PaperPosition', imPos);
+    print(h, destFilename, '-dpdf', '-r0')
+end
