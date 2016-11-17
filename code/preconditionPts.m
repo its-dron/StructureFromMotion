b6ec08fce33423a66c39cb05bbc92bd5ff2cb044
Daniel Ron <dron@alum.mit.edu>
2016-11-17 02:02:37 -0500

    Modified preconditionPts to work on N dimensional data

--- code/preconditionPts.m
+++ code/preconditionPts.m
@@ -1,17 +1,17 @@
 function [ ptsConditioned, transform ] = preconditionPts( pts, avgDist )
 %PRECONDITIONPTS Centers points around origin with avg dist sqrt(2)
-%   Returns ptsConditioned, a matrix of size 2xnPoints, the centroid of the
+%   Returns ptsConditioned, a matrix of size dxn Points, the centroid of the
 %   points is the origin and the avgDist is the distance specified (default
 %   sqrt(2).
 %
-%   transform is the 3x3 transformation matrix:
+%   transform is the (d+1)x(d+1) transformation matrix:
 %           ptsConditioned = transform * pts
 %
 %
 %   By: Brian Pugh
 
 %% Check Inputs 
-pts = pts(1:2,:);
+d = size(pts, 1);
 
 if nargin < 2
     avgDist = sqrt(2);
@@ -23,11 +23,11 @@ end
 %% Compute
 centroid = mean(pts,2);
 ptsCentered = bsxfun(@minus, pts, centroid);
-distFromOrigin = sqrt(ptsCentered(1,:).^2 + ptsCentered(2,:).^2);
+distFromOrigin = sqrt(sum(ptsCentered .* ptsCentered));
 avgDistFromOrigin = mean(distFromOrigin(:));
 scale = avgDist / avgDistFromOrigin;
 ptsConditioned = scale * ptsCentered;
-transform = [scale*eye(3,2),[-scale*centroid; 1]];
+transform = [scale*eye(d+1,d),[-scale*centroid; 1]];
 
 end
 
