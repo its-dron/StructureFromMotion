95f7e02ea05b7f4a2973d140f2c6cd5f56eb51cb
Brian Pugh <bnp117@gmail.com>
2016-11-19 17:10:12 -0500

    initial 2 view seed for multiview

--- code/SFM.m
+++ code/SFM.m
@@ -31,11 +31,38 @@ else %Compute SIFT data
         'features', 'descriptors', 'matches', 'scores');
 end
 
-%% Data Integrity Check
+%% Initial 2 view stereo
+pts1 = features{1}(1:2,matches{1,2}(1,:));
+pts2 = features{2}(1:2,matches{1,2}(2,:));
+testIm = imread(imPaths{1});
+M = size(testIm);
+M = max(M(1:2));
 
-%% Camera Pose Estimation
+[ F, bestInlierIdx ] = ransacF( pts1', pts2', M );
+[ E ] = essentialMatrix( F, Ks{1}, Ks{2} );
 
-%% Triangulation
+matches{1,2}(:, ~bestInlierIdx) = []; %delete bad matches
+pts1 = features{1}(1:2,matches{1,2}(1,:))';
+pts2 = features{2}(1:2,matches{1,2}(2,:))';
+
+M1 = Ks{1} * eye(3,4);
+M2s = camera2(E, eye(3), zeros(3,1));
+for i = 1:4
+    M2 = Ks{2}*M2s(:,:,i);
+    [ P, error ] = triangulate( M1, pts1, M2, pts2 );
+    
+    %% Check if points are in front of camera and break early if possible
+    % check if points are in front of camera
+    if any(P(:,3) < 0) == false 
+        break;
+    elseif i==4
+        error('No valid M2 !?!');
+    end
+end
+[ P, error ] = triangulate( M1, pts1, M2, pts2);
+
+%% Add images iteratively
+
 
 %% Bundle Adjustment
 
